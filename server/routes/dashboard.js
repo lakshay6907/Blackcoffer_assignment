cafe1c8cc4f07ff0941fd83e4d9f40992111d2e8
lakshay6907 <lg1690104@gmail.com>
2024-04-12 15:31:32 +0530

    ..

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,22 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Dashboard = require('../models/dashboard.model');
 
-// Fetch data with filters
+// Fetch all data
 router.get('/dashboard', async (req, res) => {
   try {
-    const { intensity, likelihood, relevance, year, country, topics, region, city } = req.query;
-    const filters = {};
-
-    if (intensity) filters.intensity = intensity;
-    if (likelihood) filters.likelihood = likelihood;
-    if (relevance) filters.relevance = relevance;
-    if (year) filters.year = year;
-    if (country) filters.country = country;
-    if (topics) filters.topics = topics;
-    if (region) filters.region = region;
-    if (city) filters.city = city;
-
-    const dashboardData = await Dashboard.find(filters);
+    const dashboardData = await Dashboard.find();
+    console.log('Fetched Data:', dashboardData);
     res.json(dashboardData);
   } catch (error) {
     res.status(500).json({ message: error.message });
